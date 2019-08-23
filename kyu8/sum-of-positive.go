64002789c979f5dbe9c451ec2fbd2198ab887dc3
danielVg <danielveragi@gmail.com>
2019-08-23 19:56:53 +0200

    kyu 8 challenges

---
+++ kyu8/sum-of-positive.go
@@ -0,0 +1,18 @@
+package kyu8
+
+// https://www.codewars.com/kata/sum-of-positive/train/go
+// PositiveSum returns the summ of all positive numbers in an array
+func PositiveSum(numbers []int) int {
+
+	// Sum of positive numbers
+	var numbersSum int
+
+	for _, e := range numbers {
+		// Check for positive numbers
+		if e > 0 {
+			numbersSum += e
+		}
+	}
+
+	return numbersSum
+}
